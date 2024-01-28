66038eda5f369e1506e728b94cbd0e3eb3e3f26e
AashishSec ఆశిష్ <65489287+aashishsec@users.noreply.github.com>
2024-01-28 12:23:29 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='subBruter',
+    version='1.0.2',
+    description='subBruter is a tool designed to efficiently probe for alive subdomins from a provided wordlist.',
+    author='Bande Aashish',
+    url='https://github.com/aashishsec/subBruter',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'httpx',
+        'colorama',
+    ],
+    extras_require={
+        'dev': ['argparse', 'concurrent.futures'],
+    },
+    entry_points={
+        'console_scripts': [
+            'subBruter=subBruter.subBruter:main',
+        ],
+    },
+)
+
 
